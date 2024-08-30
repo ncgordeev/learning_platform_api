5141c8358107f196703e2c868a154e64aaccfa9f
ncgordeev <ncgordeev@gmail.com>
2024-08-30 12:21:44 +0300

    CRUD realized

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'preview',)
+    search_fields = ('title',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'title', 'preview',)
+    search_fields = ('title',)
+    list_filter = ('course',)
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание курса')
+    preview = models.ImageField(upload_to='course/preview', default='course/preview/default.png',
+                                verbose_name='Изображение', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+    title = models.CharField(max_length=100, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание урока')
+    preview = models.ImageField(upload_to='lessons/preview', default='lessons/preview/default.png',
+                                verbose_name='Изображение', **NULLABLE)
+    url = models.URLField(verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
